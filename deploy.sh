cda295c8f891bcab2acb421d13de2e12a00baca8
Lisa Smith <lisa.smith@metalab.csun.edu>
2019-02-19 12:50:45 -0800

    Still failing... Trying to fix

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -e
 
 # This should be added to a script
+# Push image to ECR
 - export PATH=$PATH:$HOME/.local/bin
 - if [[ "$TRAVIS_BRANCH" == "docker-stuff" ]]; then
+  # Get AWS credentials for ECR
   eval $(aws ecr get-login --no-include-email --region us-west-2) ;
+  # Build, tag and push new tag of image
   docker build -t affinity:v1.5 . ;
   docker tag affinity:v1.5 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:v1.5 ;
   docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:v1.5 ;
   fi
-
-
-
-# Push image to ECR
-
