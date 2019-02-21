4d91e24203dbcdf7212902284f5a8d8b9534624e
Lisa Smith <lisa.smith@metalab.csun.edu>
2019-02-21 10:37:39 -0800

    Adding variables

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@
 # Push image to ECR
 - export PATH=$PATH:$HOME/.local/bin
 - if [[ "$TRAVIS_BRANCH" == "docker-stuff" ]]; then
-  # Get AWS credentials for ECR
   eval $(aws ecr get-login --no-include-email --region us-west-2) ;
-  # Build, tag and push new tag of image
-  docker build -t affinity:v1.5 . ;
-  docker tag affinity:v1.5 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:v1.5 ;
-  docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:v1.5 ;
+  docker build -t ${PROJECT}:${VERSION} . ;
+  docker tag ${PROJECT}:${VERSION} 973408325585.dkr.ecr.us-west-2.amazonaws.com/${PROJECT}:${VERSION} ;
+  docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:${VERSION} ;
   fi
