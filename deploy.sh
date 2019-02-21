abac279039df179572f82e566fa6026c31a8b2bb
Lisa Smith <lisa.smith@metalab.csun.edu>
2019-02-21 11:16:16 -0800

    I think this solved my token error

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 # This should be added to a script
 # Push image to ECR
-- export PATH=$PATH:$HOME/.local/bin
-- if [[ "$TRAVIS_BRANCH" == "docker-stuff" ]]; then
+export PATH=$PATH:$HOME/.local/bin
+if [[ "$TRAVIS_BRANCH" == "docker-stuff" ]]; then
   eval $(aws ecr get-login --no-include-email --region us-west-2);
   docker build -t ${PROJECT}:${VERSION} .;
   docker tag ${PROJECT}:${VERSION} 973408325585.dkr.ecr.us-west-2.amazonaws.com/${PROJECT}:${VERSION};
   docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:${VERSION};
-  fi
+ fi
