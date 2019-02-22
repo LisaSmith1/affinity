6dae0cc11b01bbf4cfb7101f4e4728abc2fd9857
Lisa Smith <lisa.smith@metalab.csun.edu>
2019-02-21 16:57:20 -0800

    Script should work

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:$HOME/.local/bin
 if [[ "$TRAVIS_BRANCH" == "docker-stuff" ]]; then
   eval $(aws ecr get-login --no-include-email --region us-west-2);
   #Lets build, tag and push our image
-  docker build -t ${PROJECT} .;
-  docker tag ${PROJECT}:${VERSION} 973408325585.dkr.ecr.us-west-2.amazonaws.com/${PROJECT}:${VERSION};
-  docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:${VERSION};
+  docker build -t ${PROJECT}:${VERSION} .;
+  docker tag ${PROJECT}:${VERSION} 973408325585.dkr.ecr.us-west-2.amazonaws.com/${ECR_REPO}:${VERSION};
+  docker push 973408325585.dkr.ecr.us-west-2.amazonaws.com/affinitytest:${ECR_REPO};
  fi
